4071b4bb19401aab8dd233524de699865b0d5517
Eddie​ <ejung716@gmail.com>
2015-12-14 10:04:29 -0800

    created public js files with authService.js; created partials folder and views folder with index.ejs

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+angular.module('goalsApp', [])
+
+// application configuration to integrate token into requests
+.config(function($httpProvider) {
+
+  // attach our auth interceptor to the http requests
+  $httpProvider.interceptors.push('authInterceptor')
+
+});
