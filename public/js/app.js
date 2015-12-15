f6d6a0c1333dbc5b9085af8943de7e55bd057a19
Eddie​ <ejung716@gmail.com>
2015-12-15 12:21:12 -0800

    login working

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-angular.module('goalsApp', ['ngAnimate', 'app.routes', 'mainCtrl', 'userCtrl', 
+angular.module('goalsApp', ['ngAnimate', 'app.routes', 'mainCtrl', 'userCtrl',
 	'userService', 'authService', 'goalsCtrl', 'goals', 'goalsFactory'])
 
 // application configuration to integrate token into requests
 .config(function($httpProvider) {
 
 	// attach our auth interceptor to the http requests
-    $httpProvider.interceptors.push('AuthInterceptor')
+    $httpProvider.interceptors.push('authInterceptor')
 });
