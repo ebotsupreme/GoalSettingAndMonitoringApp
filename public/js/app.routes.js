4eb38eb34ea01c05ad7851d55d06530dd9f3a815
Eddie​ <ejung716@gmail.com>
2015-12-15 10:55:08 -0800

    goal monitor page and form page done

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -36,6 +36,12 @@
           controller : 'placeholderController',
           controllerAs : 'placeholder'
         })
+        // goal monitor form
+        .when('/goalmonitorform', {
+          templateUrl : 'partials/goal-monitor-form.html',
+          controller : 'placeholderController',
+          controllerAs : 'placeholder'
+        })
         // goal monitor
         .when('/goalmonitor', {
           templateUrl : 'partials/goal-monitor.html',
